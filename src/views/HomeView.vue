01313a4669ab3d0328e1dba7caa0ae4418eca065
niderman <viktor.niderman@gmail.com>
2023-12-06 11:16:51 +0200

    start project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ToDoList from '../components/ToDoList.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ToDoList />
   </main>
 </template>
